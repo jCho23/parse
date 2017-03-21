f5bd9821b0376b53b1f26f2de9d409cd73b3200d
JuneCho23 <junecho.ucb@gmail.com>
2017-03-21 10:36:13 -0700

    Automation Test Scripts for Parse

---
+++ parse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace parse
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			Console.Write("Please enter your age:");
+			string yourAge = Console.ReadLine();
+
+			int yourAgeAsInt = int.Parse(yourAge);
+			yourAgeAsInt += 1;
+
+			Console.WriteLine("Your age is {0}", yourAgeAsInt.ToString());
+
+
+
+
+
+
+
+
+
+
+
+		}
+
+
+
+	}
+}
